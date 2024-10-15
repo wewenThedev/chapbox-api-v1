c3c72275b90d8b963657b75275e7160627dde69b
wewenThedev <owendalmeida1@gmail.com>
2024-10-15 14:20:36 +0100

    Ajout des routes API Resource

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MediaController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PaymentMethodController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShoppingDetailsController;
+use App\Http\Controllers\ShopProductController;
+use App\Http\Controllers\SupermarketController;
+use App\Http\Controllers\PromoController;
+use App\Http\Controllers\ShopController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +38,42 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Routes pour les utilisateurs
+Route::apiResource('users', UserController::class);
+
+// Routes pour les supermarchés
+Route::apiResource('supermarkets', SupermarketController::class);
+
+// Routes pour les produits
+Route::apiResource('products', ProductController::class);
+
+// Routes pour les commandes
+Route::apiResource('orders', OrderController::class);
+
+// Routes pour les catégories
+Route::apiResource('categories', CategoryController::class);
+
+// Routes pour les marques
+Route::apiResource('brands', BrandController::class);
+
+// Routes pour les médias
+Route::apiResource('media', MediaController::class);
+
+// Routes pour les notifications
+Route::apiResource('notifications', NotificationController::class);
+
+// Routes pour les avis
+//Route::apiResource('feedbacks', FeedbackController::class);
+
+// Routes pour les paiements
+Route::apiResource('payments', PaymentController::class);
+
+// Routes pour les cartes
+Route::apiResource('carts', CartController::class);
+
+// Routes pour les détails de shopping
+Route::apiResource('shopping-details', ShoppingDetailsController::class);
+
+// Routes pour les promotions
+Route::apiResource('promos', PromoController::class);
